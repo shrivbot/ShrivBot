27637a6be0a0c06ecc195ed37fcdcdfcea00e5e4
Ryan <Rwhytsell67@gmail.com>
2019-06-24 22:46:10 -0400

    Datalayer callbacks

--- bot/datalayer.js
+++ bot/datalayer.js
@@ -27,109 +27,6 @@ export default class DataLayer {
         })
     }
 
-    // Initialize new user
-    initUser(username) {
-        pool.connect((err, client, release) => 
-        {
-            if (err) {
-                Console.error("SEVERE: Error getting client from pool")
-                return
-            }
-            client.query("INSERT INTO user(username, score) VALUES($1, $2)", [username, 500], (err, result) => {
-                release()
-                if (err) {
-                    Console.error("SEVERE: Error initializing user")
-                    return
-                }
-                return result
-            })
-        })
-    }
-
-    // Get User score
-    getUserScore(username) {
-        pool.connect((err, client, release) => 
-        {
-            if (err) {
-                Console.error("SEVERE: Error getting client from pool")
-                return
-            }
-            client.query("SELECT * FROM user WHERE username == $1", username, (err, result) => {
-                release()
-                if(err) {
-                    Console.error("SEVERE: Error retrieving user")
-                    return err
-                }
-                if (result.rows.length == 0) {
-                    async(initUser(username))
-                    return 500
-                }
-                return result.rows[0].score
-            })
-        })
-    }
-
-    // Get User reports
-    getuserReports(username) {
-        pool.connect((err, client, release) => 
-        {
-            if (err) {
-                Console.error("SEVERE: Error getting client from pool")
-                return
-            }
-            client.query("SELECT * FROM reports WHERE username == $1", username, (err, result) => {
-                release()
-                if(err) {
-                    Console.error("SEVERE: Error retrieving user")
-                    return err
-                }
-                if (result.rows.length == 0) {
-                    async(initUser(username))
-                    return 500
-                }
-                return result.rows[0].score
-            })
-        })
-    }
-
-    // Report user
-    reportUser(reportingUsername, reportedUsername, comment) {
-
-    }
-    
-    // Praise user
-    praiseUser(reportingUsername, reportedUsername, comment) {
-
-    }
-
-    // Remove report/praise
-    removeReport(reportId) {
-
-    }
-
-    // Get Username ID
-    getUserId(username) {
-        pool.connect((err, client, release) => 
-        {
-            if (err) {
-                Console.error("SEVERE: Error getting client from pool")
-                return
-            }
-            client.query("SELECT * FROM user WHERE username == $1", username, (err, result) => {
-                release()
-                if(err) {
-                    Console.error("SEVERE: Error retrieving user")
-                    return err
-                }
-                if (result.rows.length == 0) {
-                    initUser(username)
-                    return 
-                }
-                return result.rows[0].id
-            })
-        })
-    }
-
     /*  
     Datatypes:
         User:
@@ -142,4 +39,129 @@ export default class DataLayer {
             IsPraise:Bool
             Comment:Text
     */
+
+    // Initialize new user
+    initUser(username, callback) {
+        pool.connect((err, client, release) => 
+        {
+            if (err) {
+                Console.error("SEVERE: Error getting client from pool")
+                return callback(err, null);
+            }
+            client.query("INSERT INTO User(Username, Score) VALUES($1, $2)", [username, 500], (err, result) => {
+                release()
+                if (err) {
+                    Console.error("SEVERE: Error initializing user")
+                    return callback(err, null);
+                }
+                callback(null, result)
+            })
+        })
+    }
+
+    // Get User score
+    getUserScore(username, callback) {
+        pool.connect((err, client, release) => 
+        {
+            if (err) {
+                Console.error("SEVERE: Error getting client from pool")
+                return callback(err, null);
+            }
+            client.query("SELECT Score FROM user WHERE Username == $1", username, (err, result) => {
+                release()
+                if(err) {
+                    Console.error("SEVERE: Error retrieving user")
+                    return callback(err, null);
+                }
+                if (result.rows.length == 0) {
+                    async(initUser(username))
+                    return 500
+                }
+                return result.rows[0].score
+            })
+        })
+    }
+
+    // Get User reports
+    getuserReports(username, callback) {
+        pool.connect((err, client, release) => 
+        {
+            if (err) {
+                Console.error("SEVERE: Error getting client from pool")
+                return callback(err, null);
+            }
+            client.query("SELECT * FROM reports WHERE username == $1", username, (err, result) => {
+                release()
+                if(err) {
+                    Console.error("SEVERE: Error retrieving user")
+                    return callback(err, null);
+                }
+                if (result.rows.length == 0) {
+                    async(initUser(username))
+                    callback(null, 500)
+                }
+                return result.rows[0].score
+            })
+        })
+    }
+
+    // Report user
+    reportUser(reportingUsername, reportedUsername, comment, callback) {
+        pool.connect((err, client, release) => 
+        {
+            if (err) {
+                Console.error("SEVERE: Error getting client from pool")
+                return callback(err, null);
+            }
+            client.query("SELECT * FROM reports WHERE username == $1", reportingUsername, (err, result) => {
+                if(err) {
+                    Console.error("SEVERE: Error retrieving reporting user")
+                    release()
+                    return callback(err, null);
+                }
+                if (result.rows.length == 0) {
+                    initUser(username, (err, newUser) => {
+                        if (err) return callback(err, null);;
+                        
+                    })
+                    
+                }
+                return result.rows[0].score
+            })
+            release()
+        })
+    }
+    
+    // Praise user
+    praiseUser(reportingUsername, reportedUsername, comment, callback) {
+
+    }
+
+    // Remove report/praise
+    removeReport(reportId, callback) {
+
+    }
+
+    // Get Username ID
+    getUserId(username, callback) {
+        pool.connect((err, client, release) => 
+        {
+            if (err) {
+                Console.error("SEVERE: Error getting client from pool")
+                return callback(err, null)
+            }
+            client.query("SELECT * FROM user WHERE username == $1", username, (err, result) => {
+                release()
+                if(err) {
+                    Console.error("SEVERE: Error retrieving user")
+                    return callback(err, null)
+                }
+                if (result.rows.length == 0) {
+                    var newUser = initUser(username)
+                    return callback(null, newUser.id)
+                }
+                return callback(null, result.rows[0].id)
+            })
+        })
+    }
 }
