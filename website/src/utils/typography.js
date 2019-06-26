580c549a3a920a7ec27c275c6b5fdc6d20ca11ed
Levi Butcher <lbutche3@wvup.edu>
2019-06-25 21:24:17 -0400

    Init Website

---
+++ website/src/utils/typography.js
@@ -0,0 +1,8 @@
+import Typography from 'typography'
+import parnassus from 'typography-theme-parnassus'
+
+const typography = new Typography(parnassus)
+
+// Export helper functions
+export const { scale, rhythm, options } = typography
+export default typography
