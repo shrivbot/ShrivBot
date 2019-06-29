ebe03aeb4f0c94b59cdc6c7f7740b97a0330a0f6
Levi Butcher <lbutche3@wvup.edu>
2019-06-28 22:59:21 -0400

    Workingish report

--- bot/bot.js
+++ bot/bot.js
@@ -1,6 +1,6 @@
-import { prefix, token } from "./config.json";
-import { Client } from "discord.js";
-import DataLayer from "./datalayer";
+const { prefix, token } = require("./config.json");
+const { Client } = require("discord.js");
+const DataLayer = require("./datalayer");
 
 const config = {
   connectionString:
@@ -11,112 +11,112 @@ const dl = new DataLayer(config);
 console.log(dl);
 
 const bot = new Client({
-    disableEveryone: true,
-    
-    disabledEvents: ['TYPING_START']
+  disableEveryone: true,
+
+  disabledEvents: ["TYPING_START"]
 });
 
 bot.on("ready", () => {
-    bot.user.setActivity('Observing toxicity'); 
-    console.log(`Bot is online!\n${bot.users.size} users, in ${bot.guilds.size} servers connected.`);
+  bot.user.setActivity("Observing toxicity");
+  console.log(
+    `Bot is online!\n${bot.users.size} users, in ${
+      bot.guilds.size
+    } servers connected.`
+  );
 });
 
 bot.on("guildCreate", guild => {
-    console.log(`I've joined the guild ${guild.name} (${guild.id}), owned by ${guild.owner.user.username} (${guild.owner.user.id}).`);
+  console.log(
+    `I've joined the guild ${guild.name} (${guild.id}), owned by ${
+      guild.owner.user.username
+    } (${guild.owner.user.id}).`
+  );
 });
 
-bot.on("message", async message => { 
+bot.on("message", async message => {
+  if (message.author.bot || message.system) return; // Ignore bots
 
-    if(message.author.bot || message.system) return; // Ignore bots
-    
-    if(message.channel.type === 'dm') { // Direct Message
-        return; // Where most action will occur
-    } 
+  if (message.channel.type === "dm") {
+    // Direct Message
+    return; // Where most action will occur
+  }
 
-    console.log(message.content); // Log chat to console for debugging/testing
-    
-    if (message.content.indexOf(prefix) === 0) {
-        
-        let msg = message.content.slice(prefix.length); // slice of the prefix on the message
+  console.log(message.content); // Log chat to console for debugging/testing
 
-        let args = msg.split(" "); // break the message into part by spaces
+  if (message.content.indexOf(prefix) === 0) {
+    let msg = message.content.slice(prefix.length); // slice of the prefix on the message
 
-        let cmd = args[0].toLowerCase(); // set the first word as the command in lowercase just in case
+    let args = msg.split(" "); // break the message into part by spaces
 
-        args.shift(); // delete the first word from the args
+    let cmd = args[0].toLowerCase(); // set the first word as the command in lowercase just in case
 
-        
-        if (cmd === 'report' || cmd === 'bad') { // the first command [I don't like ping > pong]
-            let targetName = args[0];// The user
-            let sender = message.member.user.id;
-            let text = args.slice(1).join(' ');
-            let result = dl.reportUser(targetName, sender, text, (err, isGood) => {
-                if(isGood) {
-                    console.log('is good')
-                } else {
-                    console.log(err)
-                }
-            })
-            return result; 
+    args.shift(); // delete the first word from the args
+
+    if (cmd === "report" || cmd === "bad") {
+      // the first command [I don't like ping > pong]
+      let targetName = args[0].slice(2, -1); // The user, removes <@> with slice
+      let sender = message.member.user.id;
+      let text = args.slice(1).join(" ");
+      let result = dl.reportUser(targetName, sender, text, (err, result) => {
+        console.log("HIT REPORT CALLBACK");
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(result);
         }
-
-        else if (cmd === 'ping') {
-            return message.channel.send('pong');
+      });
+      return result;
+    } else if (cmd === "ping") {
+      return message.channel.send("pong");
+    } else if (cmd === "getReports") {
+      let targetName = args[0]; // The user
+      dl.getuserReports(targetName);
+      return;
+    } else if (cmd === "praise") {
+      let targetName = args[0]; // The user
+      let sender = message.member.user.id;
+      let text = args.slice(1).join(" ");
+      let result = dl.praiseUser(targetName, sender, text, (err, isGood) => {
+        if (isGood) {
+          console.log("is good");
+        } else {
+          console.log(err);
         }
-
-        else if (cmd === 'getReports') {
-            let targetName = args[0]; // The user
-            dl.getuserReports(targetName)
-            return;
-        }
-
-        else if (cmd === 'praise') {
-            let targetName = args[0];// The user
-            let sender = message.member.user.id;
-            let text = args.slice(1).join(' ');
-            let result = dl.praiseUser(targetName, sender, text, (err, isGood) => {
-                if(isGood) {
-                    console.log('is good')
-                } else {
-                    console.log(err)
-                }
-            })
-            return;
-        }
-
-        else if (cmd === 'score') {
-            let targetName = args[0];// The user
-            dl.getUserScore(targetName)
-            return;
-        }
-
-        else if (cmd === 'getid') {
-            let targetName = args[0];// The user
-            dl.getUserId(targetName)
-            return  targetName;
-        }
-
-        else { // if the command doesn't match, we reply with this. It should be a help command in the future.
-            message.channel.send(`Command not found`);
-            return;
-        }
-        
-    } else if (message.content.indexOf("<@"+bot.user.id) === 0 || message.content.indexOf("<@!"+bot.user.id) === 0) { // Catch @Mentions
-        return message.channel.send(`Use \`${prefix}\` to interact with me.`); //help people learn your prefix
+      });
+      return;
+    } else if (cmd === "score") {
+      let targetName = args[0]; // The user
+      dl.getUserScore(targetName);
+      return;
+    } else if (cmd === "getid") {
+      let targetName = args[0]; // The user
+      dl.getUserId(targetName);
+      return targetName;
+    } else {
+      // if the command doesn't match, we reply with this. It should be a help command in the future.
+      message.channel.send(`Command not found`);
+      return;
     }
-    return;
+  } else if (
+    message.content.indexOf("<@" + bot.user.id) === 0 ||
+    message.content.indexOf("<@!" + bot.user.id) === 0
+  ) {
+    // Catch @Mentions
+    return message.channel.send(`Use \`${prefix}\` to interact with me.`); //help people learn your prefix
+  }
+  return;
 });
 
 // Catch Errors before they crash the app.
-process.on('uncaughtException', (err) => {
-    const errorMsg = err.stack.replace(new RegExp(`${__dirname}/`, 'g'), './');
-    console.error('Uncaught Exception: ', errorMsg);
-    // process.exit(1); //Eh, should be fine, but maybe handle this?
+process.on("uncaughtException", err => {
+  const errorMsg = err.stack.replace(new RegExp(`${__dirname}/`, "g"), "./");
+  console.error("Uncaught Exception: ", errorMsg);
+  // process.exit(1); //Eh, should be fine, but maybe handle this?
 });
 
-process.on('unhandledRejection', err => {
-    console.error('Uncaught Promise Error: ', err);
-    // process.exit(1); //Eh, should be fine, but maybe handle this?
+process.on("unhandledRejection", err => {
+  console.error("Uncaught Promise Error: ", err);
+  // process.exit(1); //Eh, should be fine, but maybe handle this?
 });
 
 bot.login(token);
--- bot/datalayer.js
+++ bot/datalayer.js
@@ -2,36 +2,32 @@ const { Pool } = require("pg");
 const { promisify } = require("util");
 let pool;
 
+module.exports = class DataLayer {
+  constructor(databaseConfig) {
+    // TODO: Setup config file with example config
 
-export default class DataLayer {
-    
-    constructor(databaseConfig){
-
-        // TODO: Setup config file with example config
-
-        if(!databaseConfig)
-        {
-            console.error("No database configuration given. Exiting")
-            process.exit(0)
-        }
-        pool = new Pool(databaseConfig);
-        pool.connect((err, client, release) => {
-            if (err) {
-                console.error("Error connecting to database", err.stack)
-                process.exit(0)
-            }
-            client.query('SELECT NOW()', (err, result) => {
-                release()
-                if (err) {
-                    console.error("Error executing config query", err.stack)
-                    process.exit(0)
-                }
-                console.log("DataLayer initialized: " + result.rows)
-            })
-        })
+    if (!databaseConfig) {
+      console.error("No database configuration given. Exiting");
+      process.exit(0);
     }
+    pool = new Pool(databaseConfig);
+    pool.connect((err, client, release) => {
+      if (err) {
+        console.error("Error connecting to database", err.stack);
+        process.exit(0);
+      }
+      client.query("SELECT NOW()", (err, result) => {
+        release();
+        if (err) {
+          console.error("Error executing config query", err.stack);
+          process.exit(0);
+        }
+        console.log("DataLayer initialized: " + result.rows);
+      });
+    });
+  }
 
-    /*  
+  /*
     Datatypes:
         User:
             ID:GUID
@@ -44,229 +40,250 @@ export default class DataLayer {
             Comment:Text
     */
 
-    // Initialize new user
-    initUser(username, callback) {
-        pool.connect((err, client, release) => 
-        {
-            if (err) {
-                console.error("SEVERE: Error getting client from pool")
+  // Initialize new user
+  initUser(username, callback) {
+    pool.connect((err, client, release) => {
+      if (err) {
+        console.error("SEVERE: Error getting client from pool");
+        return callback(err, null);
+      }
+      client.query(
+        'INSERT INTO public."user"("Username", "Score") VALUES($1, $2) RETURNING *',
+        [username, 500],
+        (err, result) => {
+          release();
+          if (err) {
+            console.error("SEVERE: Error initializing user");
+            return callback(err, null);
+          }
+          callback(null, result);
+        }
+      );
+    });
+  }
+
+  // Get User score
+  getUserScore(username, callback) {
+    pool.connect((err, client, release) => {
+      if (err) {
+        console.error("SEVERE: Error getting client from pool");
+        return callback(err, null);
+      }
+      client.query(
+        "SELECT Score FROM user WHERE Username == $1",
+        username,
+        (err, result) => {
+          release();
+          if (err) {
+            console.error("SEVERE: Error retrieving user");
+            return callback(err, null);
+          }
+          if (result.rows.length == 0) {
+            async(initUser(username));
+            return 500;
+          }
+          return result.rows[0].score;
+        }
+      );
+    });
+  }
+
+  // Get User reports
+  getuserReports(username, callback) {
+    pool.connect((err, client, release) => {
+      if (err) {
+        console.error("SEVERE: Error getting client from pool");
+        return callback(err, null);
+      }
+      client.query(
+        "SELECT * FROM reports WHERE username == $1",
+        username,
+        (err, result) => {
+          release();
+          if (err) {
+            console.error("SEVERE: Error retrieving user");
+            return callback(err, null);
+          }
+          if (result.rows.length == 0) {
+            async(initUser(username));
+            callback(null, 500);
+          }
+          return result.rows[0].score;
+        }
+      );
+    });
+  }
+
+  // Report user
+  reportUser(reportingUsername, reportedUsername, comment, callback) {
+    pool.connect(async (err, client, release) => {
+      if (err) {
+        console.error("SEVERE: Error getting client from pool");
+        return callback(err, null);
+      }
+
+      const reportingUser = await promisify(this.initUser)(reportingUsername);
+      const reportedUser = await promisify(this.initUser)(reportedUsername);
+
+      client.query(
+        'INSERT INTO public.reports("ReportedUser", "ReportingUser", "IsPraise", "Comment") VALUES($1, $2, $3, $4)',
+        [reportedUser.rows[0].ID, reportingUser.rows[0].ID, false, comment],
+        (err, result) => {
+          release();
+          if (err) {
+            console.error("SEVERE: Error reporting user");
+            return callback(err, null);
+          }
+          this.calculateScore(reportedUsername, (err, isGood) => {
+            console.log(err);
+          });
+          return callback(null, true);
+        }
+      );
+      release();
+    });
+  }
+
+  // Praise user
+  praiseUser(reportingUsername, reportedUsername, comment, callback) {
+    pool.connect(async (err, client, release) => {
+      if (err) {
+        console.error("SEVERE: Error getting client from pool");
+        return callback(err, null);
+      }
+      const reportingUser = await promisify(this.initUser(reportingUsername));
+      const reportedUser = await promisify(this.initUser(reportedUsername));
+
+      client.query(
+        "INSERT INTO reports(ReportedUser, ReportingUser, IsPraise, Comment) VALUES($1, $2, $3, $4)",
+        [reportedUser.ID, reportingUser.ID, true, comment],
+        (err, result) => {
+          release();
+          if (err) {
+            console.error("SEVERE: Error reporting user");
+            return callback(err, null);
+          }
+          calculateScore(username, _);
+          return callback(null, true);
+        }
+      );
+      release();
+    });
+  }
+
+  // Remove report/praise
+  removeReport(reportId, callback) {
+    pool.connect((err, client, release) => {
+      if (err) {
+        console.error("SEVERE: Error getting client from pool");
+        return callback(err, null);
+      }
+      client.query(
+        "DELETE FROM report WHERE id == $1",
+        reportId,
+        (err, result) => {
+          release();
+          if (err) {
+            console.error("SEVERE: Error deleting report");
+            return callback(err, null);
+          }
+
+          calculateScore(username, _);
+          return callback(null, true);
+        }
+      );
+    });
+  }
+
+  // Get Username ID
+  getUserId(username, callback) {
+    pool.connect((err, client, release) => {
+      if (err) {
+        console.error("SEVERE: Error getting client from pool");
+        return callback(err, null);
+      }
+      client.query(
+        "SELECT * FROM user WHERE username == $1",
+        username,
+        (err, result) => {
+          release();
+          if (err) {
+            console.error("SEVERE: Error retrieving user");
+            return callback(err, null);
+          }
+          if (result.rows.length == 0) {
+            initUser(username, (err, user) => {
+              if (err) {
                 return callback(err, null);
-            }
-            client.query('INSERT INTO public."user"("Username", "Score") VALUES($1, $2) RETURNING *', [username, 500], (err, result) => {
-                release()
-                if (err) {
-                    console.error("SEVERE: Error initializing user")
-                    return callback(err, null);
-                }
-                callback(null, result)
-            })
-        })
-    }
+              }
+              return callback(null, user.id);
+            });
+          }
+          return callback(null, result.rows[0].id);
+        }
+      );
+    });
+  }
 
-    // Get User score
-    getUserScore(username, callback) {
-        pool.connect((err, client, release) => 
-        {
-            if (err) {
-                console.error("SEVERE: Error getting client from pool")
+  // This is used locally to update a users score when praised, reported, or if a report is removed
+  calculateScore(username, callback) {
+    pool.connect((err, client, release) => {
+      if (err) {
+        console.error("SEVERE: Error getting client from pool");
+        return callback(err, null);
+      }
+      client.query(
+        'SELECT * FROM public.user WHERE "Username" = $1',
+        [username],
+        (err, result) => {
+          if (err) {
+            release();
+            console.error("SEVERE: Error retrieving user");
+            return callback(err, null);
+          }
+          if (result.rows.length == 0) {
+            initUser(username, (err, user) => {
+              if (err) {
                 return callback(err, null);
-            }
-            client.query("SELECT Score FROM user WHERE Username == $1", username, (err, result) => {
-                release()
-                if(err) {
-                    console.error("SEVERE: Error retrieving user")
-                    return callback(err, null);
-                }
-                if (result.rows.length == 0) {
-                    async(initUser(username))
-                    return 500
-                }
-                return result.rows[0].score
-            })
-        })
-    }
-
-    // Get User reports
-    getuserReports(username, callback) {
-        pool.connect((err, client, release) => 
-        {
-            if (err) {
-                console.error("SEVERE: Error getting client from pool")
+              }
+              return callback(null, 500);
+            });
+          }
+          let user = result.rows[0];
+          client.query(
+            'SELECT * FROM public.reports WHERE "ReportedUser" = $1',
+            [user.id],
+            (err, result) => {
+              if (err) {
+                release();
+                console.error("SEVERE: Error user reports to calculate score");
                 return callback(err, null);
-            }
-            client.query("SELECT * FROM reports WHERE username == $1", username, (err, result) => {
-                release()
-                if(err) {
-                    console.error("SEVERE: Error retrieving user")
-                    return callback(err, null);
+              }
+              let score = 500;
+              result.fields.forEach(report => {
+                if (report.IsPraise) {
+                  score += 10;
+                } else {
+                  score -= 10;
                 }
-                if (result.rows.length == 0) {
-                    async(initUser(username))
-                    callback(null, 500)
-                }
-                return result.rows[0].score
-            })
-        })
-    }
-
-    // Report user
-    reportUser(reportingUsername, reportedUsername, comment, callback) {
-        pool.connect(async (err, client, release) => 
-        {
-            if (err) {
-                console.error("SEVERE: Error getting client from pool")
-                return callback(err, null);
-            }
-
-            const reportingUser = await (promisify(this.initUser))(reportingUsername)
-            const reportedUser = await (promisify(this.initUser))(reportedUsername)
-
-            client.query('INSERT INTO public.reports("ReportedUser", "ReportingUser", "IsPraise", "Comment") VALUES($1, $2, $3, $4)', 
-                [reportedUser.rows[0].ID, reportingUser.rows[0].ID, false, comment], 
+              });
+              client.query(
+                'UPDATE public.user SET "Score" = $1 WHERE "ID" = $2',
+                [score, user.id],
                 (err, result) => {
-                    release()
-                    if(err) {
-                        console.error("SEVERE: Error reporting user")
-                        return callback(err, null);
-                    }
-                    this.calculateScore(reportedUsername, (err, isGood) => {
-                    console.log(err);
-                })
-                    return callback(null, true)
-            })
-            release()
-        })
-    }
-    
-    // Praise user
-    praiseUser(reportingUsername, reportedUsername, comment, callback) {
-        pool.connect(async (err, client, release) => 
-        {
-            if (err) {
-                console.error("SEVERE: Error getting client from pool")
-                return callback(err, null);
+                  release();
+                  if (err) {
+                    console.error("SEVERE: Error updating users score");
+                    return callback(err, null);
+                  }
+                  return callback(null, score);
+                }
+              );
             }
-            const reportingUser = await promisify(this.initUser(reportingUsername))
-            const reportedUser = await promisify(this.initUser(reportedUsername))
-
-            client.query("INSERT INTO reports(ReportedUser, ReportingUser, IsPraise, Comment) VALUES($1, $2, $3, $4)", 
-                [reportedUser.ID, reportingUser.ID, true, comment], 
-                (err, result) => {
-                    release()
-                    if(err) {
-                        console.error("SEVERE: Error reporting user")
-                        return callback(err, null);
-                    }
-                    calculateScore(username, _)
-                    return callback(null, true)
-            })
-            release()
-        })
-    }
-
-    // Remove report/praise
-    removeReport(reportId, callback) {
-        pool.connect((err, client, release) => 
-        {
-            if (err) {
-                console.error("SEVERE: Error getting client from pool")
-                return callback(err, null)
-            }
-            client.query("DELETE FROM report WHERE id == $1", reportId, (err, result) => {
-                release()
-                if(err) {
-                    console.error("SEVERE: Error deleting report")
-                    return callback(err, null)
-                }
-
-                calculateScore(username, _)
-                return callback(null, true)
-            })
-        })
-    }
-
-    // Get Username ID
-    getUserId(username, callback) {
-        pool.connect((err, client, release) => 
-        {
-            if (err) {
-                console.error("SEVERE: Error getting client from pool")
-                return callback(err, null)
-            }
-            client.query("SELECT * FROM user WHERE username == $1", username, (err, result) => {
-                release()
-                if(err) {
-                    console.error("SEVERE: Error retrieving user")
-                    return callback(err, null)
-                }
-                if (result.rows.length == 0) {
-
-                    initUser(username, (err, user) => {
-                        if (err) {
-                            return callback(err, null)
-                        }
-                        return callback(null, user.id)
-                    })
-                }
-                return callback(null, result.rows[0].id)
-            })
-        })
-    }
-
-    // This is used locally to update a users score when praised, reported, or if a report is removed
-    calculateScore(username, callback)
-    {
-        pool.connect((err, client, release) => 
-        {
-            if (err) {
-                console.error("SEVERE: Error getting client from pool")
-                return callback(err, null)
-            }
-            client.query('SELECT * FROM public.user WHERE "Username" = $1', [username], (err, result) => {
-                if(err) {
-                    release()
-                    console.error("SEVERE: Error retrieving user")
-                    return callback(err, null)
-                }
-                if (result.rows.length == 0) {
-                    initUser(username, (err, user) => {
-                        if (err) {
-                            return callback(err, null)
-                        }
-                        return callback(null, 500)
-                    })
-                }
-                let user = result.rows[0];
-                console.log(user);
-                client.query('SELECT * FROM public.reports WHERE "ReportedUser" = $1', [user.id], (err, result) => {
-                    if(err) {
-                        release()
-                        console.error("SEVERE: Error user reports to calculate score")
-                        return callback(err, null)
-                    }
-                    console.log(result);
-                    let score = 500;
-                    result.fields.forEach(report =>
-                    {
-                        if (report.IsPraise)
-                        {
-                            score += 10;
-                        } 
-                        else {
-                            score -= 10;
-                        }
-                    });
-                    client.query("UPDATE public.user SET score = $1 WHERE id == $2", [score, user.id], (err, result) => {
-                        release()
-                        if (err) {
-                            console.error("SEVERE: Error updating users score")
-                            return callback(err, null)
-                        }
-                        return callback(null, score)
-                    })
-                })
-                return callback(null, result.rows[0].id)
-            })
-        })
-    }
-}
+          );
+          return callback(null, result.rows[0].id);
+        }
+      );
+    });
+  }
+};
